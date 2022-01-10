01ae02144eefd855271df20380ff50e314559e5a
Yunjong Jeong <yunjong@kaist.ac.kr>
2022-01-10 10:49:18 +0900

    deco: Separate stdout and stderr from exec

--- deco/shepherd.go
+++ deco/shepherd.go
@@ -1,6 +1,8 @@
 package deco
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,6 +14,10 @@ var workerPath = "./worker/main.py"
 func RunWorker(code string) ([]byte, error) {
 	cmd := exec.Command(workerPath, "--stdin")
 
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		return nil, err
@@ -21,11 +27,22 @@ func RunWorker(code string) ([]byte, error) {
 		stdin.Close()
 	}()
 
-	if out, err := cmd.CombinedOutput(); err != nil {
-		log.Println("----- worker output -----\n", string(out), "\n----------\n")
+	if err := cmd.Run(); err != nil {
+		dumpOutputs(outb, errb)
 		return nil, fmt.Errorf("worker error: %s", err)
-	} else {
-		return out, nil
 	}
 
+	var doc interface{}
+	out := outb.Bytes()
+	if err := json.Unmarshal(out, &doc); err != nil {
+		dumpOutputs(outb, errb)
+		return nil, fmt.Errorf("worker output is not json: %s", err)
+	}
+
+	return out, nil
+}
+
+func dumpOutputs(outb, errb bytes.Buffer) {
+	log.Println("----- worker stdout -----\n", outb.String(), "\n----------\n")
+	log.Println("----- worker stderr -----\n", errb.String(), "\n----------\n")
 }
